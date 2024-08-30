c3e203fe4ef155aed7eb5ed7f1ce3f801fb5b382
Your Name <you@example.com>
2024-08-31 01:02:46 +0400

    bug fixed

--- src/pages/NotFound/Notfound.tsx
+++ src/pages/NotFound/Notfound.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./NotFound.module.scss";
+import styles from "./Notfound.module.scss";
 
 const NotFound: React.FC = () => {
   return (
